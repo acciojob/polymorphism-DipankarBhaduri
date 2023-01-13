510141d640a3ee002576d0406e4e50f40a4db615
Dipankar Bhaduri <110708060+DipankarBhaduri@users.noreply.github.com>
2023-01-13 10:49:10 +0530

    Initial Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,24 +2,22 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        Product p = new Product() ;
-        int add = p.product(2,4 );
-        int add_2 = p.product(2,4 , 4);
-        double ans = p.product(2.2,4.4 );
-        System.out.println (add ) ;
-        System.out.println(add_2);
-        System.out.println(ans);
+        Product p=new Product();
+        p.product(10, 20);
+        p.product(10, 20, 30);
+        p.product(10.0, 20.0);
     }
-}
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 
-class Product {
-    public int product(int x, int y) {
-        return x+y ;
-    }
-    public int product(int x, int y, int z) {
-        return x+y+z ;
-    }
-    public double product(double x, double y) {
-        return x+y ;
-    }
+
 }
