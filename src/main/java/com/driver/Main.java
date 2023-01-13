6d794ddf08930bd9cd70e030a2b8794a4de9151d
Dipankar Bhaduri <110708060+DipankarBhaduri@users.noreply.github.com>
2023-01-13 10:36:44 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,25 @@
 package com.driver;
 
 public class Main {
-
-    static class Product {
-        public int product(int x, int y) {
-            return x+y ;
-        }
-        public int product(int x, int y, int z) {
-            return x+y+z ;
-        }
-        public double product(double x, double y) {
-            return x+y ;
-        }
-    }
-
     public static void main(String[] args) {
         Product p = new Product() ;
         int add = p.product(2,4 );
         int add_2 = p.product(2,4 , 4);
         double ans = p.product(2.2,4.4 );
+        System.out.println (add ) ;
+        System.out.println(add_2);
+        System.out.println(ans);
     }
 }
 
+class Product {
+    public int product(int x, int y) {
+        return x+y ;
+    }
+    public int product(int x, int y, int z) {
+        return x+y+z ;
+    }
+    public double product(double x, double y) {
+        return x+y ;
+    }
+}
